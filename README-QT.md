5c49552d2e661c29ef3369c8a9a3c0709e3065a6
Roberto Chasipanta <maniac787@gmail.com>
2024-08-19 22:10:06 -0500

    feat: ignore files

--- README-QT.md
+++ README-QT.md
@@ -9,6 +9,25 @@ pyside6-rcc -g python resources.qrc -o resources_rc.py
 ```
 
 # GENERATE BINARY
+
 ```shell
 pyinstaller --onefile gitstats.py --name gitstats
 ```
+
+# MD 2 PDF
+
+## Ubuntu
+
+```shell
+sudo apt-get install wkhtmltopdf
+```
+
+## MAC
+
+```shell
+brew install wkhtmltopdf
+```
+
+## Windows:
+
+Descarga e instala wkhtmltopdf desde [wkhtmltopdf](www.wkhtmltopdf.org)
--- gitstats.py
+++ gitstats.py
@@ -6,6 +6,8 @@ import time
 from typing import List
 
 from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox
+from markdown_pdf import MarkdownPdf
+from markdown_pdf import Section
 
 from core import gitlog, gitblame, gitls
 from core.model.blame import Blame
@@ -25,6 +27,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def mostrar_mensaje(self):
         text = self.lineEdit.text()
         git_report(text)
+
+        input_markdown_file = 'gitstats.md'  # Nombre de tu archivo Markdown
+        output_pdf_file = 'output.pdf'  # Nombre del archivo PDF resultante
+
+        markdown_to_pdf(input_markdown_file, output_pdf_file)
         QMessageBox.information(self, 'Mensaje', "Reporte generado")
 
 
@@ -56,6 +63,17 @@ def git_report(git_repo_path):
 
     printer.print(numstat, blame)
     logging.info("--- %s seconds ---" % (time.process_time() - start_time))
+
+
+def markdown_to_pdf(input_markdown_file, output_pdf_file):
+    # Lee el archivo Markdown
+    with open(input_markdown_file, 'r') as f:
+        text = f.read()
+
+    pdf = MarkdownPdf(toc_level=2)
+    pdf.add_section(Section(text, toc=False))
+    pdf.meta["title"] = "User Guide"
+    pdf.save("guide.pdf")
 
 
 if __name__ == '__main__':
